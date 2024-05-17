94f581df14728cf78e61659db37bf9ac710c0c34
sdshaoda <sdshaoda@126.com>
2024-05-17 20:00:05 +0800

    add comment

--- readinglist-gradle/src/main/java/readinglist/ReadingListController.java
+++ readinglist-gradle/src/main/java/readinglist/ReadingListController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
+// 组件扫描自动注册为一个Bean
 @Controller
+// 请求路径映射
 @RequestMapping("/")
 public class ReadingListController {
     private ReadingListRepository readingListRepository;
@@ -20,15 +22,19 @@ public class ReadingListController {
     }
 
     @RequestMapping(value="/{reader}", method = RequestMethod.GET)
+    // 处理GET请求
     public String readersBooks(@PathVariable String reader, Model model) {
         List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null) {
+            // 设置books变量
             model.addAttribute("books", readingList);
         }
+        // 返回readingList.html的模版
         return "readingList";
     }
 
     @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+    // 处理POST请求
     public String addToReadingList(@PathVariable String reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
